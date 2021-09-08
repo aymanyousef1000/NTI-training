e105efdc0791f7d364b5cefc272b601de7914953
Ayman Yousef <55849598+aymanyousef1000@users.noreply.github.com>
2021-09-08 21:49:53 +0200

    Add files via upload

---
+++ taskes/day 4/solving with sessions/form.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Register</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>fill your application</h2>
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>"  enctype ="multipart/form-data">
+
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">Name</label>
+    <input type="text" name="name"  class="form-control" id="exampleInputName" aria-describedby="" placeholder="Enter Name">
+  </div>
+
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">Linkedin url</label>
+    <input type="text" name="url"  class="form-control" id="exampleInputName" aria-describedby="" placeholder="Enter Name">
+  </div> 
+
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">address</label>
+    <input type="text" name="address"  class="form-control" id="exampleInputName" aria-describedby="" placeholder="Enter Name">
+  </div>
+
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+  </div>
+
+
+  <div class="form-group">
+    <label for="gender">Your gender is :</label>
+    <select id="gender" name="gender">
+        <option value="male">male</option>
+        <option value="female">female</option>
+    </select>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputPassword1">Upload your profile picture</label>
+    <input type="file" name="pp" >
+  </div>
+
+  
+  <div class="form-group">
+    <label for="exampleInputPassword1">New Password</label>
+    <input type="password" name="password"   class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+ 
+  
+  <button type="submit" class="btn btn-primary">Upload</button>
+</form>
+</div>
+
+</body>
+</html>
